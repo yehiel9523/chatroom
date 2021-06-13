e949d7bec81cc1eb52ecfcfe23aab736e73e76ad
yehiel <yehiel9523@gmail.com>
2021-06-13 12:22:58 +0300

    final

--- server.mjs
+++ server.mjs
@@ -2,28 +2,29 @@ import http from 'http';
 import qs from 'querystringify';
 import fs from 'fs';
 
-const messagesString = fs.readFileSync('messages.json', {encoding: 'UTF-8'});
+const messagesString = fs.readFileSync('messages.json', { encoding: 'UTF-8' });
 const messages = JSON.parse(messagesString);
 
 const server = http.createServer(
-    (request, response) => { 
+    (request, response) => {
         const queryString = request.url.replace('/', '');
         const params = qs.parse(queryString);
 
-        if(params.message) {
-            messages.push(params.message);
+        if (params.message && params.name) {
+            messages.push({ massage: params.message, name: params.name });
             fs.writeFileSync(
-                'messages.json', 
-                JSON.stringify(messages), 
-                { encoding: 'UTF-8' }
+                'messages.json',
+                JSON.stringify(messages), { encoding: 'UTF-8' }
             );
         }
 
-        const messagesList = messages.map(message => `<li>${message}</li>`).join('');
-        let HTMLString = fs.readFileSync('index.html', {encoding: 'UTF-8'});
+        const messagesList = messages.map(item => `<li>${item.massage} ${item.name}</li>`).join('');
+        console.log(messages);
+        console.log(messagesList);
+        let HTMLString = fs.readFileSync('index.html', { encoding: 'UTF-8' });
         HTMLString = HTMLString.replace('REPLACE_ME', messagesList);
 
-        response.writeHead(200, {'Content-Type': 'text/html'});
+        response.writeHead(200, { 'Content-Type': 'text/html' });
         response.write(HTMLString);
         response.end();
     }
